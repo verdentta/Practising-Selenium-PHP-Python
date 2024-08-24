6b4a590cb96534a173d5bb13118d74b43f881279
Romeo Paul <verdentta@gmail.com>
2024-08-24 19:30:17 -0400

    First basic Selenium Code

--- first_script.py
+++ first_script.py
@@ -1,21 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+#import selenium
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service\
 
-driver = webdriver.Chrome()
+#import time package so we can delay the stuff we do or work on
+import time
 
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
-title = driver.title
+#initialize your webdriver for chrome 
+service = Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=service)
 
-driver.implicitly_wait(0.5)
 
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+#let's go to a website
+driver.get("https://www.google.ca/")
 
-text_box.send_keys("Selenium")
-submit_button.click()
-
-message = driver.find_element(by=By.ID, value="message")
-text = message.text
+#add delay to see what's going on
+time.sleep(10)
 
 driver.quit()
